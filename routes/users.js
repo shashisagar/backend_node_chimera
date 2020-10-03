ecbc26e646de2d740b0b2c5f451603ba96841a11
shashis <shashis@chimeratechnologies.com>
2020-10-03 23:42:01 +0530

    changes in users.js

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.get('/me', async (req, res) => {
+router.get('/me', (req, res) => {
   console.log("here");
   // const user = await User.findById(req.user._id).select('-password');
   // res.send(user);
