add4803851cb70ea99d1bc8b697812880b69776f
shashis <shashis@chimeratechnologies.com>
2020-10-03 23:39:56 +0530

    modify users.js for test

--- routes/users.js
+++ routes/users.js
@@ -9,9 +9,10 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.get('/me', auth, async (req, res) => {
-  const user = await User.findById(req.user._id).select('-password');
-  res.send(user);
+router.get('/me', async (req, res) => {
+  console.log("here");
+  // const user = await User.findById(req.user._id).select('-password');
+  // res.send(user);
 });
 
 router.post('/', async (req, res) => {
