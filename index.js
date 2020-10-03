032df7deb1e61a55825be143b6fad04e11c8a519
shashis <shashis@chimeratechnologies.com>
2020-10-03 23:32:20 +0530

    azure test working tested with index.js

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
-var http = require('http');
+#!/usr/bin/env node
+const winston = require('winston');
+const express = require('express');
+const app = express();
+const http = require("http");
+const socketIo = require("socket.io");
+const server = http.createServer(app);
+const io = socketIo(server);
+const {ChatMessage, validate} = require('./models/chatMessage');
+const {User} = require('./models/user');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080)
+require('./startup/logging');
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
+
+var users = {};
+io.on("connection", (socket) => {
+    socket.on('adduser', function(data) {
+        users[socket.id] = data._id;
+        updateNicknames();
+    });
+
+    socket.on("message", async (e) => {
+        const data = {
+            toId : e.to,
+            fromId : e.from,
+            is_read : e.is_read,
+            message : e.message.text
+        }
+        const chatMessage = new ChatMessage(data);
+        await chatMessage.save();
+        io.emit("message",e);
+    });  
+
+    //showing user typing
+    socket.on("typing", function(data) {
+        io.emit("typing", data);
+    });
+
+    socket.on('disconnect', function(data){
+        delete users[socket.id];
+        updateNicknames();
+    });
+
+    function updateNicknames() {
+        io.emit('usernames', users);
+    }
+});
+
+const port = process.env.PORT || 8080;
+server.listen(port, () => winston.info(`Listening on port ${port}...`));
