933baad8244f6d559b0edf7e71f619d25a369100
shashis <shashis@chimeratechnologies.com>
2020-10-04 09:07:47 +0530

    changes mongodb config

--- startup/db.js
+++ startup/db.js
@@ -2,6 +2,7 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 
 module.exports = function() {
-  mongoose.connect('mongodb://localhost/chimera_chat')
-    .then(() => winston.info('Connected to MongoDB...'));
+  mongoose.connect("mongodb://chimeramongodb:URgCaMx227JpMh9cmVMaYpbtPdAw2YQ3vwi7lRZ9fRl3yw6ly2w2Kyy6TqzJY8Lwa25JILISp1DR2WZ56jhPfg==@chimeramongodb.documents.azure.com:10255/?ssl=true", function (err, db) {
+    db.close();
+  });
 }
